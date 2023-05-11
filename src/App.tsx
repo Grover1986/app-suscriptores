de496538a6176b58ae217f10493d7a3b4f64a3b3
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-10 22:41:07 -0500

    3. Renderizando el JSX con TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,37 @@ function App() {
     // lo primero q veremos es como se maneja TypeScript para el useState
     // veremos TypeScript en los casos más típicos de React, los states, las props y los problemas q iremos encontrando
 
-    // creamos nuestro estado
-    const [number, setNumber] = useState<number | string>(5) // entre los símbolos <> le decimos q sea tipo de dato number o string
-
-    // creamos una función para cambiar el estado, es decir cambiar el número
-    const changeNumber = () => {
-        setNumber('2')  // aqui ya no nos mostraría error xq arriba le hemos dicho q la función tmb puede recibir strings
-    }
+    // aquí creamos nuestro estado, q esta vez será un array de objetos
+    const [subs, setSubs] = useState([
+        {
+            nick: 'dapelu',
+            subMonths: 3,
+            avatar: 'https://i.pravatar.cc/150?u=dapelu',
+            description: 'Dapelu hace de moderador a veces'
+        },
+        {
+            nick: 'sergio_serrano',
+            subMonths: 7,
+            avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
+        }
+    ])
 
     return (
         <div className="App">
-            {number}
-            <button onClick={changeNumber}>Change Number</button>
+            <h1>midu subs</h1>
+            <ul>
+                {
+                    subs.map(sub => {
+                        return (
+                            <li key={sub.nick}>
+                                <img src={sub.avatar} alt={`Avatar para ${sub.nick}`} />
+                                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                                <p>{sub.description?.substring(0,100)}</p>  {/* el operador ? es un Openning Chaining (operador opcional) */}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     );
 }
